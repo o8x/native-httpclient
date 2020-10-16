fe7dadc33f5ee04428b779c491260fdafbf684c9
Alex <im@println.org>
2020-10-16 16:04:40 +0800

    :white_check_mark: 增加 UNIX sock 测试

--- native_http_client_test.go
+++ native_http_client_test.go
@@ -1,13 +1,24 @@
 package nativehttpclient
 
 import (
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestUnixSock(t *testing.T) {
-    // 暂无可用的公共 API，可按照 README 进行测试
+	sock := NewUnixSock("/var/run/docker.sock")
+	post, err := sock.Post("/containers/create?name=nginx", map[string]interface{}{
+		"AutoRemove": true,
+		"Image":      "nginx",
+	})
+
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+
+	fmt.Println(post)
 }
 
 func TestTcp(t *testing.T) {
-    // 暂无可用的公共 API，可按照 README 进行测试
+	// 暂无可用的公共 API，可按照 README 进行测试
 }
